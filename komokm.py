c4df6aa4877cbfae93831157c764d80dd66aef36
Gerald Sheenan <sheenangerald@gmail.com>
2024-09-17 13:07:25 +0700

    Latihan1

--- komokm.py
+++ komokm.py
@@ -1,1 +1,11 @@
-print("Halo")
+import random
+
+def huh():
+    kedua = random.choice(["hitam", "perkasa", "goreng", "putih", "suci"])
+    mh = random.choice(["ayam", "gajah", "manusia", "beruang", "bebek"])
+    ketiga = random.choice(["menari", "menyanyi", "bermain", "dimasak", "meledak"])
+    lokasi = random.choice(["rumah", "kantor", "gedung", "sekolah", "kampus"])
+
+    print(f"Suatu Hari, {mh} {kedua} {ketiga} di {lokasi}.")
+
+huh()
