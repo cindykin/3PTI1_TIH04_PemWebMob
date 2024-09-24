54d131b95a6236dbeb7a39dc383e0628f2131e74
StevenSebastian <steven.sebastian2006@gmail.com>
2024-09-24 13:09:08 +0700

    Pertemuan5

---
+++ pertemuan5/tes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <h1>Halo Gais</h1>
+
+</head>
+<body>
+    selamat datang <?php echo $_POST["nama"] ?> <br>
+    dengan NIM : <?php echo $_POST["nim"] ?> <br>
+</body>
+</html>
