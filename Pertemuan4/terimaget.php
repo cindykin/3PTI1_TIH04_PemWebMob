7cd79bcf8a9ce55e4c419788fc2f744975659709
KeenSTL <keenstevenlim15@gmail.com>
2024-09-24 13:11:15 +0700

    Pertemuan 4 PHP

---
+++ Pertemuan4/terimaget.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang</title>
+</head>
+
+<body>
+    Selamat Datang kak <?php echo $_GET["nama"] ?> <br>
+    Dengan NIM <?php echo $_GET["nim"]; ?>
+    <p>Ini dibuat dengan GET</p>
+</body>
+
+</html>
