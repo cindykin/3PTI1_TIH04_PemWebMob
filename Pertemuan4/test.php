592fda9d725eb3cb407f69910196544e8ae96f7f
Tiana <tianatanael12@gmail.com>
2024-09-24 13:04:03 +0700

    Pertemuan 4: Form post dan get PHP

---
+++ Pertemuan4/test.php
@@ -0,0 +1,19 @@
+<?php
+if ($_POST['pw'] != "yayayaya") {
+    echo "<p>password salah maaf</p>";
+    die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <h2>Selamat Datang, <?php echo $_POST['nama']?></h2>
+    <h3>NIM: <?php echo $_POST['nim']?></h3>
+</body>
+</html>
---
+++ Pertemuan4/testget.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get</title>
+</head>
+<body>
+    <h2>Selamat Datang, <?php echo $_GET['nama']?></h2>
+    <h3>NIM: <?php echo $_GET['nim']?></h3>
+    <p>tengok urlnya</p>
+</body>
+</html>
