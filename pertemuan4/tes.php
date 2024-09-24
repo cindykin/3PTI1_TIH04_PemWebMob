723f31355822f05a37c066c2ad0d51cbf9c00d3a
braynn12 <braynmo86@gmail.com>
2024-09-24 13:10:00 +0700

    Add files via upload

---
+++ pertemuan4/tes.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang <?php echo $_POST["nama"} ?></title>
+</head>
+<body>
+    </?php
+    $password == $_POST["pw"]
+    if (password == "rahasiabanget") {
+    ?>
+    Selamat datang kak <?php echo $_POST["nama"] ?> <br>
+    dengan nim : <?php echo $_POST["nim"] ?>
+    ini dibuat dengan post.";
+
+    <?php
+    } else {
+        echo "password salah silahkan ulangi kembali";
+    }
+?>
+</body>
+</html>
