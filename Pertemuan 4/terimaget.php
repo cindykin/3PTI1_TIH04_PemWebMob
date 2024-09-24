cf8a5b7c07c55a3d5c9a6d53c10cb2b1553e62ee
Pheremya <pheremyamargaretha@gmail.com>
2024-09-24 13:08:19 +0700

    Pertemuan 4 PHP Dasar

---
+++ Pertemuan 4/terimaget.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 4</title>
+</head>
+<body>
+    Welcome miss <?php echo $_GET["name"]?><br>
+    Dengan NIM <?php echo $_GET["nim"]?>
+    ini dibuat dengan method GET
+</body>
+</html>
---
+++ Pertemuan 4/terimapost.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 4</title>
+</head>
+<body>
+    <?php
+        $password = $_POST["pw"];
+        if($password == 'rahasia'){
+    ?>
+        Welcome miss <?php echo $_POST["name"]?><br>
+        Dengan NIM <?php echo $_POST["nim"]?>
+        ini dibuat dengan method POST
+    <?php
+    }else{
+        echo "Password invalid";
+    }
+    ?>
+
+</body>
+</html>
