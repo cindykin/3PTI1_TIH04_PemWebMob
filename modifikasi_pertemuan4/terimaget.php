917cd936598b7b8f9ee0f1f008c85db5b323758a
alvitonatanael <alvitonatanael@gmail.com>
2024-10-01 20:05:59 +0700

    Add files via upload
    modifikasi index.php

---
+++ modifikasi_pertemuan4/terimaget.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat datang</title>
+</head>
+<body>
+    Selamat datang kak <?php echo $_GET["nama"] ?><br>
+    Dengan NIM : <?php echo $_GET["nim"] ?> 
+    Ini dibuat dengan method GET.
+</body>
+</html>
