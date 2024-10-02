558eb1f595f5a280a1103974e2db9d7f876a2a12
Gerald Sheenan <141151294+Geraldsheenan@users.noreply.github.com>
2024-10-02 15:55:08 +0700

    PERTEMUAN 5

---
+++ pertemuanUTS/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+$db = mysqli_connect('localhost','root','','Latihandb');
+
+if(!$db) {
+    die("gagal terhubung ke database : " . mysqli_connect_error());
+}
+
+function request ($req){
+    global $db;
+    $namatabel = mysqli_query($db,$req);
+
+    #mengembalikan array numerik berisi data kalian didalam tabel itu
+    $ambiltabel = mysqli_fetch_all($namatabel);
+
+    #array numerik dilempar lagi, nanti ke $mahasiswa di calonsiswa.php
+    return $ambiltabel;
+    
+};
+
+
+?>
