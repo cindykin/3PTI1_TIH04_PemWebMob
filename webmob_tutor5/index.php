7b844beb6a92c05b581d45797082392125fc445b
AlvianRivaldo <alvian.rivaldo23@gmail.com>
2024-10-05 12:50:58 +0700

    Pertemuan 5

---
+++ webmob_tutor5/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMP Tadika Mesra</title>
+    <style>
+        body { 
+            text-align: center 
+        }
+    </style>
+</head>
+<body>
+    <h1>Selamat Datang di Website Universitas Tadika Mesra</h1><br>
+
+    <h3>Penerimaan mahasiswa baru untuk tahun ajaran 2025/2026 akan segera dibuka</h3>
+    <p>Segera daftarkan putra putri anda untuk ke Universitas kami untuk mendapatkan masa depan yang cerah blink blink</p>
+    
+    <a href="daftar.php">Daftar</a>
+    <a href="masuk.php">Masuk</a> <br>
+
+    <a href="calonsiswa.php">List calon mahasiswa yang sudah mendaftarkan diri</a>
+    
+</body>
+</html>
