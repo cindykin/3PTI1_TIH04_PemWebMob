a9efe09e1c01a49439426c7fedc8ef3f9c18d8c5
chrisslevi0110 <christians.gracialevi@gmail.com>
2024-10-08 19:24:34 +0700

    pertemuan5

---
+++ Pertemuan5_tutor/belajarforeach.php
@@ -0,0 +1,50 @@
+<?php
+// array biasa 2d
+$mahasiswa = [
+    ["Ucil", "32251001", "ucil123@gmail.com"],
+    ["Gigi", "32201010", "gigimu@yahoo.com"]
+];
+
+// echo $mahasiswa[0][0];
+
+// foreach ($mahasiswa as $mhs) {
+//     foreach ($mhs as $m) {
+//         echo $m;
+//     }
+// };
+
+// array associative
+$mahasiswa2 = [
+    // key => value
+    [
+        "nama" => "Ucil",
+        "nim" => "32251001",
+        "email" => "ucil123@gmail.com"
+    ],
+    [
+        "nama" => "Gigi",
+        "nim" => "32251010",
+        "email" => "gigimu@yahoo.com"
+    ]
+];
+
+foreach ($mahasiswa2 as $mhs2) {
+    echo "nama : " . $mhs2['nama'] . "<br>";
+    echo "nim  : " . $mhs2['nim'] . "<br>";
+    echo "email: " . $mhs2['email'] . "<br>";
+}
+
+echo "<br><br>";
+
+foreach ($mahasiswa2 as $mhs2) {
+    foreach ($mhs2 as $key => $value) {
+        echo $key . ": " . $value . "<br>";
+    }
+}
+
+// foreach ($mahasiswa2 as $mhs2) {
+//     foreach ($mhs2 as $m2) {
+//         echo $m2;
+//     }
+// };
+?>
---
+++ Pertemuan5_tutor/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Universitas Tadika Mesra</title>
+</head>
+<body>
+    <h1>Selamat datang di website Universitas Tadika Mesra</h1> <br>
+
+    <h3>Penerimaan mahasiswa baru untuk tahun ajaran 2025/2026 akan segera dibuka</h3> <br>
+    <p>segera daftarkan putra putri anda di Universitas kami untuk mendapatkan masa depan yang cerah blink blink</p><br>
+
+    <a href="daftar.php">Daftar</a>
+    <a href="masuk.php">Masuk</a> <br>
+
+    <a href="calonsiswa.php">List calon siswa yang sudah mendaftarkan diri</a>
+</body>
+</html>
