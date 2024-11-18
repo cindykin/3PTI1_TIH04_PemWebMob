17338e6a1ca422d0df8d86ff33455d3edd8fc606
deeha <97351275+danielH3012@users.noreply.github.com>
2024-11-19 06:25:42 +0700

    Create test.dart

---
+++ test.dart
@@ -0,0 +1,19 @@
+void main(){
+  var nama = "ucok";
+  String nama_binatang = 'timothy';
+  //String [] nama_b = 'timothy';
+  print(nama);
+  print("nama binatang = $nama_binatang");
+  print(nama.toUpperCase());
+  print("nama gede ${nama_binatang.toUpperCase()}");
+
+  List warna = ['yellow','green','red'];
+  List<String> warna2 = ['yellow','green','red'];
+  Map kendaraan = {"mobil": "merci", "motor": "supra"};
+  print("warna kaos = ${warna[2]}");
+  print(kendaraan["mobil"]);
+
+  final String a;
+  a = "dijwgfiwhfwdhwdhu";
+  const String b = "fniehfuwfnahf";
+}
