6964480b6cd8af6c95f5ab8ab9ac470fd7b7ea64
Tiana <tianatanael12@gmail.com>
2024-11-19 13:01:19 +0700

    Pertemuan 10 flutter

--- flutter_tutor/lib/main.dart
+++ flutter_tutor/lib/main.dart
@@ -17,16 +17,96 @@ class AppTerbenci extends StatelessWidget {
           )
       ),
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Yeay"),
+          centerTitle: true,
+          backgroundColor: Theme.of(context).primaryColor,
+          foregroundColor: Colors.white,
+        ),
+        backgroundColor: Theme.of(context).colorScheme.surfaceBright,
         body: Center(
-          child: KartuNama(),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                space(),
+                Text("ini logo flutter"),
+                FlutterLogo(size: 100,),
+                space(),
+                Text("ini kartu"),
+                KartuIntro(),
+                space(),
+                ElevatedButton(
+                  onPressed: (){ 
+                    print("ow");
+                  }, 
+                  child: Text("yey aku button"),
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.secondary),
+                    foregroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.onSecondary),
+            
+                  ),
+                  ),
+                  space(),
+                  Text("aku icon-icon"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.account_circle,
+                        size: 50,
+                        ),
+                      Icon(
+                        Icons.downhill_skiing,
+                        size: 50,
+                        ),
+                      Icon(
+                        Icons.handshake_outlined,
+                        size: 50,
+                        ),
+                      Icon(
+                        Icons.skateboarding_outlined,
+                        size: 50,
+                        ),
+                    ],
+                  ),
+                  space(),
+                  Text("ini miraikan"),
+                  SizedBox(
+                    width: 330,
+                    height: 440,
+                    child: Image.network(
+                      "https://pbs.twimg.com/media/GaRJi9kbUAIqBD1?format=jpg&name=large",                    
+                      ),
+                  ),
+                  space(),
+                  Text("ini mocchi :D"),
+                  SizedBox(
+                    width: 330,
+                    child: Image.asset("images/mocchi.png")
+                  )
+                ]
+              ),
+          ),
         ),
       )
     );
   }
 }
 
-class KartuNama extends StatelessWidget {
-  const KartuNama({
+class space extends StatelessWidget {
+  const space({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: 50,);
+  }
+}
+
+class KartuIntro extends StatelessWidget {
+  const KartuIntro({
     super.key,
   });
 
@@ -34,13 +114,19 @@ class KartuNama extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       color: Theme.of(context).colorScheme.primary,
-      elevation: 10,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(
-          "halo halo halo",
-          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
-          ),
+      child: Card(
+        color: Theme.of(context).colorScheme.onPrimary,
+        elevation: 10,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            "halo halo halo",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+              ),
+            ),
+        ),
       ),
     );
   }
