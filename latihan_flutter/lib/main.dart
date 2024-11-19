52c59f3ff3aff65d64ad3d9eec79d46b1a29acfc
Pheremya <pheremyamargaretha@gmail.com>
2024-11-19 14:09:25 +0700

    Latihan Dart 2

--- latihan_flutter/lib/main.dart
+++ latihan_flutter/lib/main.dart
@@ -9,10 +9,55 @@ class AppTesting extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 153, 186, 243),
-      body: Text('hello'),
-    ));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi random"),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(210, 240, 199, 255),
+        ),
+        backgroundColor: const Color.fromARGB(255, 153, 186, 243),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Menambahkan pengaturan alignment
+              children: [
+                Text("Hello World", style: TextStyle(fontSize: 24)),
+                SizedBox(height: 20), // Jarak antar elemen
+                Container(
+                  width: 300, // Mengurangi ukuran container
+                  height: 300, // Mengurangi ukuran container
+                  color: Colors.red,
+                  alignment: Alignment.center,
+                  child: const Text("ini adalah container",
+                      textAlign: TextAlign.center),
+                ),
+                SizedBox(height: 20), // Jarak antar elemen
+                FlutterLogo(size: 200),
+                SizedBox(height: 20), // Jarak antar elemen
+                ElevatedButton(
+                  onPressed: () {
+                    print("tombol di klik");
+                  },
+                  child: const Text("ini adalah button"),
+                ),
+                SizedBox(height: 20), // Jarak antar elemen
+                Icon(
+                  Icons.add_alarm,
+                  size: 150,
+                  color: Colors.white,
+                ),
+                SizedBox(height: 20), // Jarak antar elemen
+                Image.asset("gambar/moomin.jpg"), // Dari lokal
+                SizedBox(height: 20), // Jarak antar elemen
+                Image.network(
+                    "https://tse4.mm.bing.net/th?id=OIP.0tF9x5Z_ddTXzKpNDbKpNwHaEK&pid=Api&P=0&h=220"), // Dari internet
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
