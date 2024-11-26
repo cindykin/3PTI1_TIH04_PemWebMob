6fbbda0a6ed95a1e998bc00e78068478f3a73869
Paskalis Nunang <130380785+paskal26@users.noreply.github.com>
2024-11-26 10:05:47 +0700

    tutor2

--- main.dart
+++ main.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AppCinta());
+  runApp(App2());
 }
 
-class AppCinta extends StatelessWidget {
+class App2 extends StatelessWidget {
+  const App2({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green,
-        body: Text('Halo World'),
-      )
-    );
+        //papan kayu
+        home: Scaffold(
+      //canvas
+      appBar: AppBar(
+        title: Text("APP BARU"),
+        centerTitle: true,
+        backgroundColor: Colors.yellow,
+      ),
+      backgroundColor: Colors.green,
+      body: Center(
+        // child: Text("Hello WORLD"),
+        // child: Container(
+        //     width: 100,
+        //     height: 100,
+        //     color: Colors.blue,
+        //     child: Center(child: Text("container"))
+        // ),
+        // child: ElevatedButton(
+        //   onPressed: () {
+        //     print("tombol diklik")
+        //   },
+        //   child: Text("Button example"),
+        // ),
+        child: Icon(
+          size: 150,
+          Icons.ac_unit_rounded,
+          color: Colors.white,
+        ),
+      ),
+    ));
   }
 }
